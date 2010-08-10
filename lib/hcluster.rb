9a14da9ca969a7fabdb30d917d4e57ec299272f8
Eugene Koontz <eugene_koontz@trendmicro.com>
2010-08-10 13:52:18 -0700

    fix typo in user feedback message.

--- lib/hcluster.rb
+++ lib/hcluster.rb
@@ -181,7 +181,7 @@ module Hadoop
       hbase_thread.join
       hadoop_thread.join
 
-      puts "Tarballs uploaded. You can now call 'build_image' on this object."
+      puts "Tarballs uploaded. You can now call 'create_image' on this object."
     end
 
     # ami-b00c34d9 => rightscale-us-east/RightImage_CentOS_5.4_x64_v5.4.6.2_Beta.manifest.xml
