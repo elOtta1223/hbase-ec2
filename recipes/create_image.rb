c5a3388f8f7078fc4d9755f95f050ef49306f369
Eugene Koontz <ekoontz@hiro-tan.org>
2010-09-23 12:26:02 -0700

    create-image recipe

---
+++ recipes/create_image.rb
@@ -0,0 +1,13 @@
+$:.unshift("~/hbase-ec2/lib")
+load 'hcluster.rb'
+include Hadoop 
+
+options = {
+  :hadoop_url => 'http://ekoontz-tarballs.s3.amazonaws.com/hadoop-0.20-tm-3.tar.gz',
+  :hbase_url => 'http://ekoontz-tarballs.s3.amazonaws.com/hbase-0.20-tm-3.tar.gz', 
+  :tar_s3 => "ekoontz-tarballs", 
+  :ami_s3 => "ekoontz-amis"
+}
+
+builder = Himage.new options
+builder.create_image :debug => true
