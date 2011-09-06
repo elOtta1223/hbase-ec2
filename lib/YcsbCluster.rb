c62a3ed09d7b3d5127be7361f3950f102452e6dc
Mingjie Lai <mingjie_lai@trendmicro.com>
2011-09-06 14:27:51 -0700

    test on mothership

--- lib/YcsbCluster.rb
+++ lib/YcsbCluster.rb
@@ -1,13 +1,184 @@
-require 'hcluster.rb'
+
+require 'rubygems'
 require 'net/smtp'
+require 'monitor'
+require 'net/ssh'
+require 'net/scp'
+require 'AWS'
+require 'aws/s3'
 
 module Hadoop
 
-class YcsbCluster < SecureCluster
+class YcsbCluster # < SecureCluster
+  @master = ""
+  @slaves = []
+  @launchOptions = {}
   
-  def launch(options = {})
-    super(options)
+  HBASE_ROOT_DIR="/usr/lib/hbase"  
+  MASTER_HOST_FILE="/tmp/ec2-master"
+  
+  EC2_ROOT_SSH_KEY = ENV['EC2_ROOT_SSH_KEY'] ? "#{ENV['EC2_ROOT_SSH_KEY']}" : "#{ENV['HOME']}/.ec2/ec2-user.pem"
+  EC2_CERT = ENV['EC2_CERT'] ? "#{ENV['EC2_CERT']}" : "#{ENV['HOME']}/.ec2/cert.pem"
+  EC2_PRIVATE_KEY = ENV['EC2_PRIVATE_KEY'] ? "#{ENV['EC2_PRIVATE_KEY']}" : "#{ENV['HOME']}/.ec2/key.pem"
+
+  if ENV['AWS_ENDPOINT'] != nil
+    ENDPOINT = ENV['AWS_ENDPOINT']
+  else
+    ENDPOINT = "ec2.amazonaws.com"
   end
+  
+  def initialize()
+  end
+  
+  def getMaster
+    file = File.open( "#{MASTER_HOST_FILE}", "rb" )
+    master = file.read
+    return master.gsub("\n", "")
+  end
+    
+  def launch(options = {})    
+    File.delete("#{MASTER_HOST_FILE}") if (File.exist?("#{MASTER_HOST_FILE}"))
+    @launchOptions = options 
+
+    # launch master
+    %x[ #{@launchOptions[:scriptHome]}/bin/launch-master ]
+    
+    # wait until image started
+    p "Wait for master starting: "
+    while (!File.exist?(MASTER_HOST_FILE))
+      p "."
+      sleep(5)
+    end
+    
+    @master = getMaster
+    
+    # launch slaves
+    %x[ #{@launchOptions[:scriptHome]}/bin/launch-slaves #{@master} #{@launchOptions[:slaveNumber]} ]
+    
+    timeOutCounter=0
+    slaveString = ""
+    while (true)
+      sleep(6)
+      
+#      slaveString = %x[ #{@launchOptions[:scriptHome]}/bin/list-slaves #{@master}]
+      slaveString = %x[ #{ENV["EC2_HOME"]}/bin/ec2-describe-instances --region us-west-1 | grep running | awk '{print $4}' | grep -v #{@master} ]
+      @slaves = slaveString.split("\n") if slaveString != nil
+      
+      if (@slaves.length == @launchOptions[:slaveNumber])
+        continue=false
+        @slaves.each {|s| 
+          if (!s.start_with?("ec2"))
+            continue=true
+          end
+        }
+        break if !continue
+      end
+      return false if (++timeOutCounter > 200)
+    end
+    p "Master: " + @master
+    p "Slaves: " + @slaves.to_s
+  end
+  
+  def terminate()
+    connection = AWS::EC2::Base.new(
+      :access_key_id=>ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key=>ENV['AWS_SECRET_ACCESS_KEY'],
+      :server => ENDPOINT) 
+    
+    if (connection.describe_instances.reservationSet.length > 0)  
+      connection.describe_instances.reservationSet.item.each { |instance| 
+        options = {
+          :instance_id => instance.instancesSet.item[0].instanceId
+        }
+        connection.terminate_instances options
+      }
+    end
+  end
+  
+  def echo_stdout
+    return lambda{|line,channel|
+      puts line
+    }
+  end
+  
+  def echo_stderr 
+    return lambda{|line,channel|
+      puts "(stderr): #{line}"
+    }
+  end
+  
+  def ssh(command = nil,
+          stdout_line_reader = echo_stdout,
+          stderr_line_reader = echo_stderr,
+          host = @master,
+          begin_output = nil,
+          end_output = nil)
+    ssh_with_host(command,stdout_line_reader,stderr_line_reader,host,begin_output,end_output)
+  end
+
+  def ssh_to(host,
+             command=nil,
+             stdout_line_reader = echo_stdout,
+             stderr_line_reader = echo_stderr,
+             begin_output = nil,
+             end_output = nil)
+    ssh_with_host(command,stdout_line_reader,stderr_line_reader,host,begin_output,end_output)
+  end
+  
+  def ssh_with_host(command,stdout_line_reader,stderr_line_reader,host,begin_output,end_output)
+    if command == nil
+      interactive = true
+    end
+    
+    if begin_output
+      print begin_output
+      STDOUT.flush
+    end
+    # http://net-ssh.rubyforge.org/ssh/v2/api/classes/Net/SSH.html#M000013
+    # paranoid=>false because we should ignore known_hosts, since AWS IPs get frequently recycled
+    # and their servers' private keys will vary.
+    
+    until command == "exit\n"
+      if interactive == true
+        print "#{host} $ "
+        command = gets
+      end
+      Net::SSH.start(host,'ec2-user',
+                           :keys => [EC2_ROOT_SSH_KEY],
+                           :paranoid => false
+                           ) do |ssh|
+        stdout = ""
+        channel = ssh.open_channel do |ch|
+          channel.request_pty()
+          channel.exec(command) do |ch, success|
+            #FIXME: throw exception(?)
+            puts "channel.exec('#{command}') was not successful." unless success
+          end
+          channel.on_data do |ch, data|
+            stdout_line_reader.call(data,channel)
+            # example of how to talk back to server.
+            #          channel.send_data "something for stdin\n"
+          end
+          channel.on_extended_data do |channel, type, data|
+            stderr_line_reader.call(data,channel)
+          end
+          channel.wait
+          if !(interactive == true)
+            #Cause exit from until(..) loop.
+            command = "exit\n"
+          end
+          channel.on_close do |channel|
+            # cleanup, if any..
+          end
+        end
+      end
+    end
+    if end_output
+      print end_output
+      STDOUT.flush
+    end
+  end
+
 
   # Send results to email recipients thru SMTP
   def sendResults(reportDetail, options)
@@ -56,7 +227,7 @@ MESSAGE_END
     formattedTime = testEndTime.strftime("%Y-%m-%d %H:%M:%S %z")
     reportDetail += "Test end time: #{formattedTime}<br>\n"
     
-    reportDetail += "# of RS: #{@num_regionservers}<br>\n"
+    reportDetail += "# of RS: #{@slaves.length}<br>\n"
     reportDetail += "HBase version: #{buildNumber}<br>\n"
     reportDetail += "HBase compiled time: #{builtTime}<br>\n"
     reportDetail += "Git changes: #{gitChanges.to_s}<br>\n"
@@ -87,17 +258,28 @@ MESSAGE_END
     return reportDetail
   end
 
+  def installYCSB(options = {})
+    ssh(". #{HBASE_ROOT_DIR}/conf/hbase-env.sh; rm -rf #{options[:remoteYcsbDir]};" +
+          "mkdir -p #{options[:remoteYcsbDir]}; " +
+          "cd #{options[:remoteYcsbDir]}; " +
+          "wget -nv #{options[:ycsbURL]}; " +
+          "tar xfP ycsb.tar;" + 
+          "cd ycsb; rm -rf run-test*; " +
+          "wget https://s3.amazonaws.com/mlai.hadoop.tarballs/run-test.sh;" +
+          "chmod +x run-test.sh; sudo cp #{HBASE_ROOT_DIR}/conf/hbase.keytab hbase.keytab; " + 
+          "sudo chown ec2-user hbase.keytab;")
+  end
+    
     
   # perform test and analyze the results (including sending emails) 
   def test(options = {}, gitChanges = nil)
+    installYCSB options
+    
     # perform test
     testStartTime = Time.new
     # 1. run ycsb test
-    ssh(". /usr/local/hbase/conf/hbase-env.sh; rm -rf #{options[:remoteYcsbDir]};" +
-      "mkdir -p #{options[:remoteYcsbDir]}; cd #{options[:remoteYcsbDir]}; " +
-      "wget -nv #{options[:ycsbURL]}; " +
-      "tar xfP ycsb.tar;" + 
-      "cd ycsb; chmod +x run-test.sh;" +
+    ssh("cd #{options[:remoteYcsbDir]}/ycsb; echo create \\\"usertable\\\", \\\"family\\\" > init; echo exit >>init;" + 
+      "kinit -k -t ./hbase.keytab hbase/`hostname -f`;kinit -R; hbase shell init;" +
       "#{options[:remoteYcsbDir]}/ycsb/run-test.sh " + 
       "-p recordcount=#{options[:testRecordCount]} " + 
       "-p operationcount=#{options[:testOperationCount]} " + 
@@ -111,8 +293,8 @@ MESSAGE_END
       
     # 2. download the results files locally.
     %x[mkdir -p #{options[:localProcessDir]}; ]
-    Net::SCP.start(dnsName(),
-      'root',
+    Net::SCP.start(@master,
+      'ec2-user',
       :keys => ENV['EC2_ROOT_SSH_KEY'],
       :paranoid => false
       ) do |scp|
@@ -124,12 +306,12 @@ MESSAGE_END
   
     %x[rm -rf #{jarDire}; mkdir -p #{jarDire}]
     
-    Net::SCP.start(dnsName(),
-      'root',
+    Net::SCP.start(@master,
+      'ec2-user',
       :keys => ENV['EC2_ROOT_SSH_KEY'],
       :paranoid => false
       ) do |scp|
-        scp.download! "/usr/local/hbase/#{options[:jarFileName]}", jarDire
+        scp.download! "#{HBASE_ROOT_DIR}/#{options[:jarFileName]}", jarDire
     end
     
     %x[cd #{jarDire}; jar xf #{options[:jarFileName]}]
--- recipes/ycsb.rb
+++ recipes/ycsb.rb
@@ -7,16 +7,17 @@ ycsbTestOptions = {
   :emailTo => ["mingjie_lai@trendmicro.com", "mingjie@gmail.com"],
   :emailFrom =>  "ycsb.hbase@gmail.com",  
   :localProcessDir => "/tmp/ycsb",
-  :remoteYcsbDir => "/mnt/ycsb",
-  :testRecordCount => "1000",
-  :testOperationCount => "1000",
+  :remoteYcsbDir => "/home/ec2-user/ycsb",
+  :testRecordCount => "100000",
+  :testOperationCount => "100000",
   :testThreadNumber => "10",
   :smtpServer => "smtp.gmail.com",
   :smtpAccount => "ycsb.hbase",
   :smtpPassword => "Qazwsx123_",
   # a customized ycsb, containing a run script and modified logic.
   :ycsbURL => "https://s3.amazonaws.com/mlai.hadoop.tarballs/ycsb.tar",
-  :jarFileName => "hbase-0.90-tm-5.jar"  
+  :ycsbScript => "https://s3.amazonaws.com/mlai.hadoop.tarballs/run-test.sh",
+  :jarFileName => "hbase-0.90-tm-5+1.jar"  
 }
 
 #  ["apurtell@apache.org",
@@ -28,36 +29,25 @@ GitOptions = {
   # check scm, to determine whether there is code changes since last
   # time. true: check scm and rebuild jar, if there is changes, rebuild
   # jar and use the new jar. false: to perform tests without scm check. 
-  :userDefaultImage => true,
+  :userDefaultImage => false,
   :gitWorkDir => "/home/mlai/git/hbase.asf",
-  :gitCOBranch => "tm-5",
+  :gitCOBranch => "tm-5+1",
   :gitCheckLogSince => "10",
   :gitRemote => "origin",
   :gitRepo => "git@github.com:trendmicro/hbase-private.git",
   :jarFileName => "hbase-0.90-tm-5.jar"  
 }
 
-ClusterOptions = {
-  :label => "hbase-0.90-tm-5-x86_64",
-  :master_instance_type => "m1.large",
-  :rs_instance_type => "c1.xlarge",
-  :zk_instance_type => "m1.large",
-  :num_zookeepers => 1,
-  :num_regionservers => 3,
-  :security_group_prefix => "hcluster",
-  :debug_level => 1,
-  :hbase_debug_level => "INFO",
-  :owner_id => '801535628028'
-}
-
 @ycsb = Hadoop::YcsbCluster
 
-cluster = @ycsb.new ClusterOptions
+cluster = @ycsb.new
 
 launchOptions = {
-  :availability_zone => 'us-west-1c',
-  :uploadHBaseJarDire => ""
+  :scriptHome => "/home/mlai/git/tm-ec2/",
+  :slaveNumber => 3
 }
+
+#cluster.launch launchOptions
 
 #if there is scm changes
 
@@ -104,12 +94,16 @@ def getGitChangesSinceLastTime(workDir, repo, remote, branch, lastTime)
       p "Open git dire: " + workDir + ". Start to pull from remote/branch: " + 
         remote + "/" + branch
       #g.pull(remote, branch)
-      g.fetch(remote)
-      g.merge(remote)
-      g.branch(branch)
-      p "Check changes from " + lastTime.strftime("%Y-%m-%d %H:%M:%S %z")
-      changes = g.log().since(lastTime.strftime("%Y-%m-%d %H:%M:%S %z")).to_s
-      return changes
+      begin
+	      g.fetch(remote)
+	      g.merge(remote)
+	      g.branch(branch)
+	      p "Check changes from " + lastTime.strftime("%Y-%m-%d %H:%M:%S %z")
+	      changes = g.log().since(lastTime.strftime("%Y-%m-%d %H:%M:%S %z")).to_s
+	      return changes
+	  rescue => e
+	  	p "Git ailed to work. " + e.message
+	  end
     end
   end  
 end
@@ -179,7 +173,6 @@ begin
         t = Time.new
         writeBuildTimeToFile(buildTimeFile, t)
         
-        launchOptions[:uploadHBaseJarDire] = localJarPath
         cluster.launch launchOptions
       else
         # No need to start a test.
@@ -199,7 +192,7 @@ begin
     cluster.launch launchOptions
   end
   
-  
+  #cluster.installYCSB ycsbTestOptions
   cluster.test ycsbTestOptions, gitChanges
   
 rescue => e
